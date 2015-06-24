adb2f7c69c5a6eaa0579a5294eadb4370591e6b3
jsp <jspenkar@gmail.com>
2015-06-23 22:51:07 -0500

    4 of 5 checks working.

--- js/components/app.js
+++ js/components/app.js
@@ -24,24 +24,26 @@ var App = module.exports = React.createClass({
 	render: function(){
 		var variable = this.state.variable, array=[], errors = [];
     var password = this.state.password;
-		if(password.length < variable.lengths){
-			errors.push(<li>Password is not long enough.</li>)
-		}	
-		if(password.match(/[a-zA-Z]/g).length < variable.letters){
-			errors.push(<li>Password does not contain enough Letters.</li>)
-		}	
-		// if(password.match(/\d/g).length < variable.numbers){
-		// 	errors.push(<li>Password does not contain enough numbers.</li>)
-		// }	
-		// if(password.length < variable.lengths){
-		// 	errors.push(<li>Password is not long enough.</li>)
-		// }	
-		// if(password.match(/[A-Z]/g) < variable.capitals){
-		// 	errors.push(<li>Password is not long enough.</li>)
-		// }	
-		for(var i in variable){
-			array.push(<div>{i}-=-{variable[i]}</div>);
-		}
+    if(password){
+      if(password.length < variable.lengths){
+  			errors.push(<li>Password is not long enough.</li>)
+  		}
+  		if(password.match(/[a-zA-Z]/g).length < variable.letters){
+  			errors.push(<li>Password does not contain enough Letters.</li>)
+  		}
+  		if(password.match(/\d/g).length < variable.numbers){
+  			errors.push(<li>Password does not contain enough numbers.</li>)
+  		}
+  		// if(password.length < variable.lengths){
+  		// 	errors.push(<li>Password is not long enough.</li>)
+  		// }
+  		if(password.match(/[a-zA-Z]/g).length < variable.capitals){
+  			errors.push(<li>Password does not have enough capital letters.</li>)
+  		}
+    }
+		// for(var i in variable){
+		// 	array.push(<div>{i}-=-{variable[i]}</div>);
+		// }
 		return(
 			<div>
 				<input id="password" value={variable.length} onChange={this.change} onKeyDown={this.press}/>
