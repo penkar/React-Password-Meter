654735bb525e14eed73958a8040e9b72cfae875e
jsp <jspenkar@gmail.com>
2015-06-30 21:01:41 -0500

    Header added.

--- js/body.js
+++ js/body.js
@@ -1,10 +1,13 @@
 var App = require('./components/app.js');
 var PassForm = require('./components/passform.js');
+var Header = require('./components/header.js');
+
 
 var Body = React.createClass({
 	render: function(){
 		return (
 			<div id="wrapper">
+				<Header />
 				<App />
 				<PassForm />
 			</div>
---
+++ js/components/header.js
@@ -0,0 +1,7 @@
+var Header = module.exports = React.createClass({
+  render: function(){
+    return (
+      <div id="header" className="clearfix">React Password Strength RegEx</div>
+    )
+  }
+})
